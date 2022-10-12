c563f704198da63a2c2e9111685d3ec0647e8728
aldintana <aldin.tanovic@edu.fit.ba>
2022-10-12 16:35:33 +0200

    update permissionInsertRequest

--- User-Managment/Application/Requests/Permissions/PermissionInsertRequest.cs
+++ User-Managment/Application/Requests/Permissions/PermissionInsertRequest.cs
@@ -1,8 +1,12 @@
-﻿namespace Application.Requests.Permissions
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Requests.Permissions
 {
     public class PermissionInsertRequest
     {
+        [Required(AllowEmptyStrings = false)]
         public string Code { get; set; }
+        [Required(AllowEmptyStrings = false)]
         public string Description { get; set; }
     }
 }
